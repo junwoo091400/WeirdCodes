22b7ed0598fc363cd116767016d2a3b06f3d84c6
junwoo091400 <junwoo091400@gmail.com>
2019-01-07 14:16:10 +0900

    Added helloyonsei.com Index.php File

---
+++ CollegeAdmissionPage/Yonsei_helloyonsei/index.php
@@ -0,0 +1,121 @@
+<!--
+Access Date : 2019 / 01/ 07 (Monday)
+
+Original Link : helloyonsei.com
+
+Why this is Interesting :
+
+1) Basically because 'SIT' students wrote it, and is obviously Hand-Made.
+
+2) It's Yonsei univ.
+
+3) When I tried to enter "index.html", it directed me to "http://errdoc.gabia.net/404.html", so that's cool too.
+
++ aand, just some extra info from : "https://xn--c79as89aj0e29b77z.xn--3e0b707e/kor/whois/whois.jsp"
+
+Admin Name : Yontem
+
+Name Server : josh.ns.cloudflare.com
+
+Creation Date : 2017-11-16T12:30:09Z
+
+Registrar WHOIS Server : whois.gabia.com
+-->
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<title>연세대학교 합격자 인증 시스템</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+<div class="header my-4 pl-1" style="max-width: 520px; height: auto;">
+<img class="img-fluid" src="./images/admission-logo.png">
+</div>
+<div class="jumbotron text-left mb-5">
+<h3>안녕하십니까, 19학번 새내기 여러분!</h3>
+<br>
+<p class="lead">여러분들의 연세대학교 합격을 진심으로 축하드립니다.</p>
+<p> 저희는 연세대학교 중앙새내기맞이단으로 19학번 새내기 여러분들의 안전하고 즐거운 새내기 생활을 위하여 결성된 유일한 공식 단체입니다. 본 단체는 학교 중앙 소속 단체로서 신입생 여러분들의 안정적인 학교생활 적응을 위하여 각종 업무를 진행하고 있습니다.</p>
+<p>그동안 매년 합격자 발표 철마다 연세대학교에서는 신입생의 신분을 사칭하는 사건이 발생해 논란이 일었습니다. 그런 사건을 미리 예방하고자 저희 19학년도 중앙새내기맞이단은 글로벌융합공학부 학생들과 협력하여 ‘합격자 인증 시스템’을 개발하여 운영하고 있습니다. 아래 지시 사항에 따라 이름과 수험번호 등의 인증 정보를 기입하신 뒤 <mark>합격자 인증하기</mark> 버튼을 누르시면 자동으로 합격자 인증이 진행됩니다.</p>
+<p>적어주신 메일로 연세대학교 19학번 공식 새내기 카페(네이버)와 연세대학교 19학번 공식 새내기 그룹(페이스북)의 초대메일이 발송될 예정입니다. 24시간 이내에 메일이 오지 않을 경우 우선적으로 네이버 카페메일함을 확인해주시고, 메일함에도 없을 경우 <a href="https://www.facebook.com/yonseifreshman">연세대학교 중앙새내기맞이단 페이스북 페이지</a>로 메시지 바랍니다.</p>
+<br>
+<p>
+<em>
+<strong>기입하신 정보는 안전하게 보관되며 인증 절차를 마친 후 완전히 폐기됩니다.</strong>
+</em>
+</p>
+</div>
+<form action="./index.php" method="post" class="mt-5">
+<div class="form-group row marketing">
+<label for="name-input" class="col-sm-3 col-form-label">이름 <small>Name</small></label>
+<div class="col-sm-9">
+<input class="form-control" type="text" name="name" id="name-input" value="" placeholder="(예) 홍길동" required>
+</div>
+</div>
+
+<div class="form-group row">
+<label for="admissions-type-input" class="col-sm-3 col-form-label">입학 전형 <small>Admis.</small></label>
+<div class="col-sm-9">
+<select class="form-control" name="type" required>
+<option selected>전형을 선택해주세요</option>
+<option value="1">수시모집(추가 합격자 포함)</option>
+<option value="5">재외국민 전형 3년특례 (예체능 계열 제외)</option>
+<option value="6">재외국민 전형 3년특례 (예체능 계열)</option>
+<option value="7">재외국민 전형 12년특례 </option>
+<option value="8">재외국민 전형 12년특례 글로벌인재학부</option>
+</select>
+</div>
+</div>
+<div class="form-group row">
+<label for="id-input" class="col-sm-3 col-form-label pl-2">수험번호 <small>Aplc. No. </small></label>
+<div class="col-sm-9">
+<input class="form-control" type="text" name="id" id="id-input" value="" placeholder="(예) EAAA12345" required>
+</div>
+</div>
+<div class="form-group row">
+<label for="birthdate-input" class="col-sm-3 col-form-label pl-2">생년월일 <small>Birthdate </small></label>
+<div class="col-sm-9">
+<input class="form-control" type="number" name="birthdate" id="birthdate-input" value="" placeholder="(예) 000101" required>
+</div>
+</div>
+<div class="form-group row">
+<label for="email_naver-input" class="col-sm-3 col-form-label pl-2">네이버 이메일 <small>Naver Email </small></label>
+<div class="col-sm-9">
+<input class="form-control" type="email_naver" name="email_naver" id="email_naver-input" value="" placeholder="(예) abc123@naver.com" aria-describedby="email-naver-help-block">
+</div>
+</div>
+<div class="form-group row">
+<label for="facebook_id-input" class="col-sm-3 col-form-label pl-2">페이스북 아이디 <small>Facebook ID </small></label>
+<div class="col-sm-9">
+<input class="form-control" type="facebook_id" name="facebook_id" id="facebook_id-input" value="" placeholder="(예) facebook123@naver.com">
+</div>
+<div class="my-4 px-3">
+<p id="email-naver-help-block" class="form-text text-muted">
+이메일을 통해 19학번 공식 네이버 카페 초대 메일이 발송되므로 꼭 올바른 주소를 입력해 주세요. <br>
+네이버 아이디와 페북 아이디 둘 중 하나만 있는 경우, 있는 아이디만 입력해주세요. (네이버, 페이스북 중 적어도 한 개 이상 계정을 만들어 주세요.) <br>
+아이디가 이메일일 경우, 19학번 공식 페이스북 그룹으로 자동초대됩니다. <br>
+아이디가 이메일이 아닐 경우, '연세대학교 중앙새내기맞이단' 페이스북 페이지로 메세지를 보내주시면 가입을 도와드리겠습니다.
+</p>
+</div>
+</div>
+<div class="text-center my-4">
+<button type="submit" class="btn btn-lg btn-primary text-center">합격자 인증하기</button>
+</div>
+</form>
+<script>
+                </script>
+<footer class="footer py-5">
+<hr />
+<img src="./images/yonsei-sit-logo.png" height="42" />
+<p class="float-right">
+이 페이지는 공과대학
+<a href="http://sit.yonsei.ac.kr">글로벌융합공학부</a> 학생들이 제작하였습니다.
+</p>
+</footer>
+</div>
+</body>
+</html>
